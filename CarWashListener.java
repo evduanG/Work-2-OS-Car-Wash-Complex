248761b0c3be4b66dcb8071b51591a31254a1ab1
Eviatar Duany <88398626+evduanG@users.noreply.github.com>
2022-03-24 21:56:06 +0200

    Rename Car Wash Listener Method

--- CarWashListener.java
+++ CarWashListener.java
@@ -1,6 +1,7 @@
 import java.util.EventListener;
 
 public interface CarWashListener extends EventListener {
+	
+	public void actionListener(CarWashEvent event);
 
-	public void printProgressMessage(CarWashEvent event);
 }
